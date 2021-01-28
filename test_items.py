3e372c87f1a15d21af5e99d8577c2789d6b46f48
asaoppenheimer <asa.oppenheimer@gmail.com>
2021-01-28 17:10:37 +0200

    added assert to test_items

--- test_items.py
+++ test_items.py
@@ -1,6 +1,12 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+import time
 
 
-def test_guest_should_see_login_link(browser):
+
+def test_guest_should_see_cart(browser):
     browser.get(link)
-    browser.find_element_by_css_selector('[type="submit"]')
+    time.sleep(10)
+    cart_button = browser.find_element_by_css_selector('[type="submit"]')
+    #assert cart_button == True, "Cart is not presented"
+    assert cart_button is not None, "Login link is not presented"
+
